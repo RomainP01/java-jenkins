24b491ec611f040f74925d93e6f46f54d86f06ab
Rhummboy <romainpanii@gmail.com>
2022-01-06 20:49:50 +0100

    ajout de la création de panier et ajout de livres dedans

--- src/main/java/com/tomyoannromain/javajenkins/Main.java
+++ src/main/java/com/tomyoannromain/javajenkins/Main.java
@@ -1,7 +1,27 @@
 package com.tomyoannromain.javajenkins;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        ArrayList<Integer> shoppingCart = createShoppingCart();
+        System.out.println(shoppingCart);
+        addHarryPotter(5,3,shoppingCart);
+        System.out.println(shoppingCart);
     }
+
+    public static ArrayList<Integer> createShoppingCart(){
+        ArrayList<Integer> shoppingCart = new ArrayList<Integer>();
+        for(int i=0;i<5;i++){
+            shoppingCart.add(0,0);
+        }
+        return shoppingCart;
+    }
+
+    public static void addHarryPotter(int numberOfBooks, int harryPotterVolume, ArrayList<Integer> shoppingCart){
+        shoppingCart.add(harryPotterVolume-1, numberOfBooks);
+    }
+
 }
--- src/test/java/com/tomyoannromain/javajenkins/TestMain.java
+++ src/test/java/com/tomyoannromain/javajenkins/TestMain.java
@@ -2,12 +2,32 @@ package com.tomyoannromain.javajenkins;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+
+import static com.tomyoannromain.javajenkins.Main.addHarryPotter;
+import static com.tomyoannromain.javajenkins.Main.createShoppingCart;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestMain {
 
     @Test
-    public void testmain(){
-        assertEquals(7,7);
+    public void testCreateArrayListOfInteger(){
+        ArrayList<Integer> shoppingCart = createShoppingCart();
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        assertEquals(shoppingCart,arrayList);
     }
+
+    @Test
+    public void testShoppingCartContains0HarryPotter(){
+        ArrayList<Integer> shoppingCart = createShoppingCart();
+        assertTrue(shoppingCart.isEmpty());
+    }
+
+    @Test
+    public void testShoppingCartContains1HarryPotter1(){
+        ArrayList<Integer> shoppingCart = createShoppingCart();
+        addHarryPotter(1,1,shoppingCart);
+        assertEquals(1,shoppingCart.get(0));
+    }
+
 }
