972535a059689c30ff295bd9d0d97909063037ff
Rhummboy <romainpanii@gmail.com>
2022-01-06 16:44:16 +0100

    first commit

---
+++ src/test/java/com/tomyoannromain/javajenkins/TestMain.java
@@ -0,0 +1,13 @@
+package com.tomyoannromain.javajenkins;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMain {
+
+    @Test
+    public void testmain(){
+        assertEquals(7,7);
+    }
+}
